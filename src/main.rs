5ab29e0d4b3212a6f3d84611817a89afbe0bb7c4
Subsequent <software@subseq.io>
2024-03-19 10:50:27 +0000

    Added functionality to forward requests to the language model API

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use reqwest::{Client, Error};
+use serde::Deserialize;
+use serde_json::Value;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().route("/language_model", web::post().to(language_model_handler)))
@@ -14,4 +17,16 @@ async fn language_model_handler(req_body: web::Json<LanguageModelRequest>) -> im
 #[derive(Deserialize)]
 struct LanguageModelRequest {
     input_text: String,
+}
+async fn forward_to_language_model(api_endpoint: &str, input_data: &Value) -> Result<(), Error> {
+    let client = Client::new();
+    let res = client.post(api_endpoint).json(input_data).send().await?;
+
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+
+    // Optionally, handle the response body
+    // let response_body = res.text().await?;
+
+    Ok(())
 }
